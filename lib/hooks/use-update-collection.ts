2387342cb9aaa234df26ea6eaef67c036531e140
H-JuKyung <hjk000hjk17@gmail.com>
2026-02-15 22:59:27 +0900

    feat(collection): 컬렉션 수정 mutation 훅 구현 [WAPO-179]

---
+++ lib/hooks/use-update-collection.ts
@@ -0,0 +1,48 @@
+"use client";
+
+import {
+  useMutation,
+  useQueryClient,
+  type UseMutationOptions,
+} from "@tanstack/react-query";
+import type { AxiosError } from "axios";
+import type {
+  UpdateCollectionRequest,
+  UpdateCollectionResponse,
+  UpdateCollectionParams,
+} from "@/types/collection";
+import { updateCollection } from "../api/collection";
+import { ProblemDetail } from "@/types/problem-detail";
+
+type Variables = {
+  collectionId: UpdateCollectionParams["collectionId"];
+  body: UpdateCollectionRequest;
+};
+
+type Options = Omit<
+  UseMutationOptions<
+    UpdateCollectionResponse,
+    AxiosError<ProblemDetail>,
+    Variables
+  >,
+  "mutationFn"
+>;
+
+export const useUpdateCollection = (options?: Options) => {
+  const queryClient = useQueryClient();
+
+  return useMutation<
+    UpdateCollectionResponse,
+    AxiosError<ProblemDetail>,
+    Variables
+  >({
+    mutationFn: ({ collectionId, body }) =>
+      updateCollection(collectionId, body),
+    ...options,
+    onSuccess: (data, variables, onMutateResult, context) => {
+      queryClient.invalidateQueries({ queryKey: ["collections"] });
+
+      options?.onSuccess?.(data, variables, onMutateResult, context);
+    },
+  });
+};
